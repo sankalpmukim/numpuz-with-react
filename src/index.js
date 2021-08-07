13a709bdd3776ee08db818b2330f8e64b564425b
sankalpmukim <sankalpmukim@gmail.com>
2021-08-07 12:08:50 +0530

    Game win working

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const generateInitArray = (gridSize) => {
   const initArray = Array.from({ length: gridSize * gridSize }, (_, index) =>
     String(index + 1)
   );
+  // initArray[initArray.length - 1] = initArray.length - 1;
+  // initArray[initArray.length - 2] = "##";
+  // return initArray;
   initArray[initArray.length - 1] = "##";
   initArray.sort(() => Math.random() - 0.5);
   return initArray;
@@ -19,12 +22,43 @@ const swapArrayElements = (arr, indexA, indexB) => {
   return arr;
 };
 
+const arraysEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
+
 const Game = () => {
   const [gridSize, setGridSize] = useState(4);
   const [squares, setSquares] = useState(generateInitArray(gridSize));
+  const [won, setWon] = useState(false);
   useEffect(() => {
     setSquares(generateInitArray(gridSize));
   }, [gridSize]);
+  useEffect(() => {
+    const newSquares = squares.slice();
+    if (newSquares.indexOf("##") === -1) {
+      setWon(true);
+      return;
+    }
+    const SIZE = Math.sqrt(newSquares.length);
+    const sortedArray = Array.from({ length: SIZE * SIZE }, (_, index) =>
+      String(index + 1)
+    );
+    newSquares[newSquares.indexOf("##")] = String(newSquares.length);
+    if (arraysEqual(newSquares, sortedArray)) {
+      console.log("Game won");
+      setWon(true);
+      return;
+    }
+    console.log("Game not won");
+    setWon(false);
+  }, [squares]);
 
   const movable = (idx) => {
     const mover = squares.indexOf("##");
@@ -94,7 +128,7 @@ const Game = () => {
 
   const handleClick = (i) => {
     // console.log(i);
-    if (movable(i) !== 0) {
+    if (movable(i) !== 0 && won === false) {
       if (movable(i) === 1) {
         verticalChange(i, squares.indexOf("##"));
       } else {
@@ -121,6 +155,17 @@ const Game = () => {
               />
             </h3>
           </label>
+          <div>
+            <h3>{won ? "Game won" : "Game not won"}</h3>
+          </div>
+          <div>
+            <button
+              className="my-button"
+              onClick={() => setSquares(generateInitArray(gridSize))}
+            >
+              Reset board
+            </button>
+          </div>
         </div>
         <Board
           squares={squares}
